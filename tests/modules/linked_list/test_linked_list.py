659c673cc34724f773ffab638c03bf123e422d3a
Gabriel Majivu <gabriel@angaza.com>
2020-05-12 15:10:38 +0300

    nit: fix test names

--- tests/modules/linked_list/test_linked_list.py
+++ tests/modules/linked_list/test_linked_list.py
@@ -4,7 +4,7 @@ from modules.linked_list import LinkedList, Node
 
 
 class TestLinkedList:
-    def test__initialize__is_empty(self):
+    def test_init__with_node__ok(self):
         first_node = Node("fake data")
         assert first_node.data == "fake data"
         assert first_node.next_element is None
@@ -12,19 +12,19 @@ class TestLinkedList:
         alist = LinkedList(first_node)
         assert alist.head_node == first_node
 
-    def test__get_head_node__ok(self):
+    def test_get_head__ok(self):
         node = Node("a")
         alist = LinkedList(node)
         assert alist.get_head() == node
 
-    def test__is_empty__ok(self):
+    def test_is_empty__empty_list__ok(self):
         alist = LinkedList()
         node = Node("a")
         blist = LinkedList(node)
         assert alist.is_empty() is True
         assert blist.is_empty() is False
 
-    def test_list_empty__insert_at_head__ok(self):
+    def test_insert_at_head__empty_list__ok(self):
         alist = LinkedList()
         assert alist.is_empty() is True
         assert alist.get_head() is None
@@ -34,7 +34,7 @@ class TestLinkedList:
         assert alist.get_head().data == data
         assert str(alist) == f"{data} -> None"
 
-    def test_list_not_empty__insert_at_head__ok(self):
+    def test_insert_at_head__non_empty_list__ok(self):
         alist = LinkedList(Node("1"))
         assert alist.is_empty() is False
         assert alist.get_head() is not None
@@ -44,7 +44,7 @@ class TestLinkedList:
         assert alist.get_head().data == data
         assert str(alist) == f"{data} -> 1 -> None"
 
-    def test_list_empty__insert_at_tail__ok(self):
+    def test_insert_at_tail__empty_list__ok(self):
         alist = LinkedList()
         assert alist.is_empty() is True
         assert alist.get_head() is None
@@ -54,7 +54,7 @@ class TestLinkedList:
         assert alist.get_head().data == data
         assert str(alist) == f"{data} -> None"
 
-    def test_list_not_empty__insert_at_tail__ok(self):
+    def test_insert_at_tail__non_empty_list__ok(self):
         alist = LinkedList(Node("1"))
         assert alist.is_empty() is False
         assert alist.get_head() is not None
